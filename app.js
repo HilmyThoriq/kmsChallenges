c66e3c4931de6f64d1f6da0dd08085067b916a8b
HilmyThoriq <hilmythoriq09@gmail.com>
2023-08-15 18:31:02 +0700

    express cikarang

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 const express = require("express");
+var cors = require('cors');
+const ipModel = require("./ip.model");
 const app = express();
 const port = process.env.PORT || 3001;
 
+app.use(cors());
 app.get("/", (req, res) => res.type('html').send(html));
+app.get("/location", (req, res) => res.json({ location: "cikarang" }));
+app.get("/visitor", async (req, res) => {
+  let ipData = new ipModel({
+   ip_addr: req.ip,
+   date_added: new Date(),
+  });
+  await ipData.save();
 
+  let numVisits = await ipModel.count()
+
+  return res.json({ your_ip: req.ip, visitor_number: numVisits });
+ });
+ 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 server.keepAliveTimeout = 120 * 1000;
